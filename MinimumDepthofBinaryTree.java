f0a21c7b1b8900cce6299dc2a56a5cd8e2d8a4e2
isax <isax@users.noreply.github.com>
2015-09-05 21:26:14 -0700

    Update MinimumDepthofBinaryTree.java

--- MinimumDepthofBinaryTree.java
+++ MinimumDepthofBinaryTree.java
@@ -24,4 +24,12 @@ public class Solution {
             return Math.min(minDepth(root.left)+1, minDepth(root.right)+1);
         }
     }
+    
+    //Short 1:
+    public int minDepth(TreeNode root) {
+       if(root==null) return 0;
+       int left = minDepth(root.left);
+       int right = minDepth(root.right);
+       return Math.min(left, right)==0? Math.max(left, right)+1 : Math.min(left,right)+1;
+    }
 }
