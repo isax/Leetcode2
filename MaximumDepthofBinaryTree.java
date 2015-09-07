67b4c0e5a179a10972082299854db10ef040a10b
isax <isax@users.noreply.github.com>
2015-09-06 21:31:16 -0700

    Update MaximumDepthofBinaryTree.java

--- MaximumDepthofBinaryTree.java
+++ MaximumDepthofBinaryTree.java
@@ -14,4 +14,35 @@ public class Solution {
         int right = maxDepth(root.right)+1;
         return Math.max(left, right);
     }
+    
+    public int maxDepthIteration(TreeNode root) {
+        if(root==null) return 0;
+        
+        Queue<TreeNode> queue  = new LinkedList<TreeNode>();
+        
+        queue.add(root);
+        int lastLevelNumOfNodes = 1;
+        int level = 0;
+        
+        while(!queue.isEmpty()){
+            level++;
+            int curLevelNumOfNodes = 0;
+            
+            for(int i = 1; i<=lastLevelNumOfNodes; i++){ // iterate in the current level
+                TreeNode node = queue.poll();
+                if(node.left!=null){
+                    queue.offer(node.left);
+                    curLevelNumOfNodes++;
+                }
+                
+                if(node.right!=null){
+                    queue.offer(node.right);
+                    curLevelNumOfNodes++;
+                }
+            }
+            lastLevelNumOfNodes = curLevelNumOfNodes;
+        }
+        return level;
+    }
+    
 }
