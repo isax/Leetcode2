7233d43efea2e27165e3a5b8f35585ba35d98648
isax <isax@users.noreply.github.com>
2015-09-14 22:32:38 -0700

    Update BestTimetoBuyandSellStock.java

--- BestTimetoBuyandSellStock.java
+++ BestTimetoBuyandSellStock.java
@@ -1,10 +1,26 @@
 public class Solution {
-    public int maxProfit(int[] prices) {
+    // code ganker: http://blog.csdn.net/linhuanmars/article/details/23162793
+    public int maxProfit(int[] prices){
+        int global = 0;
+        int local = 0;
+        
+        for(int i = 0; i<prices.length-1; i++){
+            local += prices[i+1] - prices[i];
+            if(local<0){
+                local = 0;
+            }
+            global = Integer.max(global, local);
+        }
+        return global;
+    }
+    
+    // My own solution: local and global solution
+    public int maxProfit2(int[] prices) {
         int globalProfix = 0;
         int localMin = Integer.MAX_VALUE; int localMax = Integer.MIN_VALUE;
         
         for(int i = 0; i<prices.length; i++){
-            if(prices[i]<localMin){
+            if(prices[i]<localMin){ // the same as cur profit sum < 0
                 localMin = prices[i];
                 localMax = prices[i];
             }
