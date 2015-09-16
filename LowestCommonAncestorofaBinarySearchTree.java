cf3033dd075b36c9c7a22da1ce0635b2adedf460
isax <isax@users.noreply.github.com>
2015-09-15 22:11:56 -0700

    Create LowestCommonAncestorofaBinarySearchTree.java

---
+++ LowestCommonAncestorofaBinarySearchTree.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null) return null;
+        
+        if((root.val>=p.val && q.val>=root.val) || (root.val<=p.val && q.val<=root.val)){
+            return root;
+        }else if (root.val-p.val<0){
+            return lowestCommonAncestor(root.right, p, q);
+        }else{
+            return lowestCommonAncestor(root.left, p, q);
+        }
+    }
+}
