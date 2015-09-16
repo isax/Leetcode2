23da8b37a52017e78b8e9d583d5e39485b78d1aa
isax <isax@users.noreply.github.com>
2015-09-15 21:56:18 -0700

    Update SymmetricTree.java

--- SymmetricTree.java
+++ SymmetricTree.java
@@ -18,3 +18,33 @@ public class Solution {
             return left==null&&right==null;
         }
         
+        return left.val==right.val && helper(left.left, right.right) && helper(left.right, right.left);
+    }
+    
+     // STACK ACCEPT NULL
+    public boolean isSymmetricIterative(TreeNode root) {
+       if(root==null) return true;
+       
+       Stack<TreeNode> stack = new Stack<TreeNode>();
+       
+       stack.push(root.left); 
+       stack.push(root.right);
+       
+       while(!stack.isEmpty()){
+           TreeNode right = stack.pop();
+           TreeNode left = stack.pop();
+           
+           if(left==null && right==null){
+               continue;
+           } 
+           
+           if(left ==null || right==null || left.val!=right.val){
+               return false;
+           }
+
+           stack.push(left.left); stack.push(right.right);
+           stack.push(left.right); stack.push(right.left);
+       }
+       return true;
+    }
+}
