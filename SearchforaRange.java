7acf22c4718e502503d9876326acc1200e6f11e8
isax <isax@users.noreply.github.com>
2015-09-25 21:51:30 -0700

    Create SearchforaRange.java

---
+++ SearchforaRange.java
@@ -0,0 +1,39 @@
+public class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int [] res = new int[2];
+        
+        res[0] = searchStart(nums, target, 0, nums.length-1);
+        res[1] = searchEnd(nums, target, 0, nums.length-1);
+        
+        return res;
+    }
+    
+    private int searchStart(int[]nums, int target, int left, int right){
+        boolean f = false;//int i = -1;
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            
+            if(nums[mid]<target){
+                left = mid+1;
+            }else if(nums[mid]>=target){
+                right = mid-1;
+            }
+            if(nums[mid]==target) f = true;
+        }
+        return f?right+1:-1;
+    }
+    
+    private int searchEnd(int[]nums, int target, int left, int right){
+        boolean f = false;
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            if(nums[mid]<=target){
+                left = mid+1;
+            }else {
+                right = mid-1;
+            }
+            if(nums[mid]==target) f = true;
+        }
+        return f?left-1:-1;
+    }
+}
