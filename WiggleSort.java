21ff0eca6d3a2e3086fdb5a8053e7b1352a44782
isax <isax@users.noreply.github.com>
2015-10-17 21:23:18 -0700

    Create WiggleSort.java

---
+++ WiggleSort.java
@@ -0,0 +1,16 @@
+public class Solution {
+    // https://leetcode.com/discuss/57120/4-lines-o-n-c
+    public void wiggleSort(int[] nums) {
+        for(int i = 0; i<nums.length-1; i++){
+            if(i%2==0 && nums[i]>=nums[i+1] || i%2==1 && nums[i]<=nums[i+1]){
+                swap(nums, i, i+1); 
+            }
+        }
+    }
+    
+    private void swap(int[]nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
