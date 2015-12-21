0ab2fe8cda55fc4ee9de044e49208c4dcb53650c
isax <isax@users.noreply.github.com>
2015-12-20 17:15:19 -0800

    Create 2_FlipGame.java

---
+++ 2_FlipGame.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public List<String> generatePossibleNextMoves(String s) {
+        List<String> res = new ArrayList<String>();
+        
+        for(int i = 0; i<s.length()-1; i++){
+            if(s.charAt(i)=='+' && s.charAt(i+1)=='+'){
+                res.add(s.substring(0, i)+"--"+s.substring(i+2));
+            }
+        }
+        return res;
+    }
+}
