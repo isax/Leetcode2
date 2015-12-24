15b67f15779e781bb8f9b1cc0a487d3a5ffe7b82
isax <isax@users.noreply.github.com>
2015-12-23 22:07:29 -0800

    Create 1111_2_PaintHouse.java

---
+++ 1111_2_PaintHouse.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public int minCost2(int[][] costs) {
+        int n = costs.length;
+        int [][] dp = new int[n+1][3];
+        
+        for(int i = 1; i<=n; i++){
+            dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + costs[i-1][0];
+            dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + costs[i-1][1];
+            dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + costs[i-1][2];
+        }
+        return Math.min(dp[n][0], Math.min(dp[n][1], dp[n][2]));
+    }
+    
+    public int minCost(int[][] costs) {
+        int n = costs.length;
+        int a = 0, b = 0, c = 0;
+        
+        for(int i = 1; i<=n; i++){
+            int aa = a, bb = b;
+            a = Math.min(b, c) + costs[i-1][0];
+            b = Math.min(aa, c) + costs[i-1][1];
+            c = Math.min(aa, bb) + costs[i-1][2];
+        }
+        return Math.min(a, Math.min(b, c));
+    }
+}
