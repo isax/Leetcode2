50f18e35e80ec0761fa04ddae4e665006a25968a
isax <isax@users.noreply.github.com>
2016-02-05 22:54:38 -0800

    Create 2222_FirstBadVersion.java

---
+++ 2222_FirstBadVersion.java
@@ -0,0 +1,21 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        return bs(1, n);
+    }
+    
+    private int bs(int low, int high){
+        if(low>high) return low;
+        
+        int mid =low+(high-low)/2;
+        
+        if(isBadVersion(mid)){
+            return bs(low, mid-1);
+        }else{
+            return bs(mid+1, high);
+        }
+    }
+    
+}
