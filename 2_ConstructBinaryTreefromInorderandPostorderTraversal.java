fa38aa759e8fd17b7c613278847ddd7b0b3f1170
isax <isax@users.noreply.github.com>
2016-02-10 08:28:58 -0800

    Update 2_ConstructBinaryTreefromInorderandPostorderTraversal.java

--- 2_ConstructBinaryTreefromInorderandPostorderTraversal.java
+++ 2_ConstructBinaryTreefromInorderandPostorderTraversal.java
@@ -8,6 +8,27 @@
  * }
  */
 public class Solution {
+    // 02/10
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0; i<inorder.length; i++){
+            map.put(inorder[i], i);
+        }
+        return helper(preorder, 0, preorder.length-1, inorder, 0, inorder.length-1, map);
+    }
+    
+    private TreeNode helper(int[]preorder, int p, int q, int[]inorder, int i, int j, Map<Integer, Integer> map){
+        if(p>q) return null;
+        TreeNode root = new TreeNode(preorder[p]);
+        if(p==q) return root;
+        
+        int r = map.get(preorder[p]);
+        root.left = helper(preorder, p+1, r-i+p, inorder, i, r-1, map);
+        root.right = helper(preorder, r-i+p+1, q, inorder, r+1, j, map);
+        return root;
+    }
+
+    
     public TreeNode buildTree(int[] inorder, int[] postorder) {
         return helper(inorder, 0, inorder.length-1, postorder, 0, postorder.length-1);
     }
