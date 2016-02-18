b7d93406f663126a372924bd95cefbae2f5e3ce8
isax <isax@users.noreply.github.com>
2016-02-17 22:50:34 -0800

    Update 2_IsomorphicStrings.java

--- 2_IsomorphicStrings.java
+++ 2_IsomorphicStrings.java
@@ -1,5 +1,25 @@
 public class Solution {
     public boolean isIsomorphic(String s, String t) {
+        Map<Character, Character> hm = new HashMap<Character, Character>();
+        Set<Character> hs = new HashSet<Character>();
+        
+        for(int i = 0; i<s.length(); i++){
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+            if(hm.containsKey(c2)){
+                if(hm.get(c2)!=c1) return false;
+            }else{
+                if(hs.contains(c1)) return false;
+                else{
+                    hm.put(c2, c1);
+                    hs.add(c1);
+                }
+            }
+        }
+        return true;
+    }
+    
+    public boolean isIsomorphic(String s, String t) {
         char[] map = new char[256];
         char[] map2 = new char[256];
         
