6a41762b39bada794dc86bed9a5c022e968f81c8
isax <isax@users.noreply.github.com>
2016-02-20 09:34:36 -0800

    Update 2222_ClosestBinarySearchTreeValue.java

--- 2222_ClosestBinarySearchTreeValue.java
+++ 2222_ClosestBinarySearchTreeValue.java
@@ -8,6 +8,40 @@
  * }
  */
 public class Solution {
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int closestValue(TreeNode root, double target) {
+        if(root==null) return Integer.MIN_VALUE;
+        int [] closest = new int[1];
+        closest[0] = root.val;
+        helper(root, target, closest);
+        return closest[0];
+    }
+    
+    private void helper(TreeNode root, double target, int [] closest){
+        if(root==null) return;
+        if(root.val==target) {
+            closest[0] = root.val;
+            return;
+        }
+        if(Math.abs(target-root.val)<Math.abs(target-closest[0])) closest[0] = root.val;
+        if(root.val>target){
+            helper(root.left, target, closest);
+        }else{
+            helper(root.right, target, closest);
+        }
+    }
+}
+    
+    
     // https://leetcode.com/discuss/60089/clean-and-concise-java-solution
     public int closestValue(TreeNode root, double target) {
         int prev = root.val;
