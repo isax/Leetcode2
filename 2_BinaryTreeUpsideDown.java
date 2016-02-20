dbb7bb6a88ac49b9e86612bcf15be7088dff23b0
isax <isax@users.noreply.github.com>
2016-02-19 22:54:25 -0800

    Create 2_BinaryTreeUpsideDown.java

---
+++ 2_BinaryTreeUpsideDown.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        if(root==null || root.left==null) return root;
+        //TreeNode left = root.left;
+        TreeNode newRoot = upsideDownBinaryTree(root.left);
+        root.left.right = root;
+        root.left.left = root.right;
+        root.left = null;
+        root.right= null;
+        return newRoot;
+    }
+}
