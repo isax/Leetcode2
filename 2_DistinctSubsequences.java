35af83b03c7e3868806fdc4cb8fb5443fe9eb204
isax <isax@users.noreply.github.com>
2016-02-22 09:04:07 -0800

    Create 2_DistinctSubsequences.java

---
+++ 2_DistinctSubsequences.java
@@ -0,0 +1,23 @@
+public class Solution {
+    // http://blog.csdn.net/linhuanmars/article/details/23589057
+    // https://leetcode.com/discuss/48208/lines-solutions-with-detailed-explanations-time-and-space
+    public int numDistinct(String s, String t) {
+        int len1 = s.length();
+        int len2 = t.length();
+        int [][] dp = new int[len1+1][len2+1];
+        for(int i = 0; i<=len1; i++){
+            dp[i][0] = 1;
+        }
+        
+        for(int i = 1; i<=len1; i++){
+            for(int j = 1; j<=len2; j++){
+                if(s.charAt(i-1)==t.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[len1][len2];
+    }
+}
