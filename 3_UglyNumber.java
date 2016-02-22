1435ba8530c2b693147776ee22abda08a6bb8acb
isax <isax@users.noreply.github.com>
2016-02-22 09:21:03 -0800

    Update 3_UglyNumber.java

--- 3_UglyNumber.java
+++ 3_UglyNumber.java
@@ -13,21 +13,4 @@ public class Solution {
         }
         return num==1;
     }
-    
-    // nao dong qing qi
-    public boolean isUglyBFS(int num) {
-        if(num<=0) return false;
-        Queue<Integer> q = new LinkedList<Integer>();
-        q.offer(1);
-        
-        while(!q.isEmpty()){
-            int i = q.poll();
-            if(i==num) return true;
-            
-            if(i*2<=num) q.offer(i*2);
-            if(i*3<=num) q.offer(i*3);
-            if(i*5<=num) q.offer(i*5);
-        }
-        return false;
-    }
 }
