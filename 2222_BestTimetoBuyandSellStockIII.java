8437cc7b323b209d14b51314c44c1d54c6f653cb
isax <isax@users.noreply.github.com>
2016-02-27 21:47:59 -0800

    Update and rename BestTimetoBuyandSellStockIII.java to 2222_BestTimetoBuyandSellStockIII.java

--- 2222_BestTimetoBuyandSellStockIII.java
+++ 2222_BestTimetoBuyandSellStockIII.java
@@ -1,5 +1,25 @@
 public class Solution {
     
+    public int maxProfit(int[] prices) {
+        int len = prices.length;
+        if(len==0) return 0;
+        
+        int [][] local = new int[len][3];
+        Arrays.fill(local[0], -prices[0]);
+        int [][] global = new int[len][3];
+        
+        for(int i = 1; i<len; i++){
+            int diff = prices[i]-prices[i-1];
+            
+            for(int j = 1; j<3; j++){
+                local[i][j] = Math.max(global[i-1][j-1] + Math.max(0, diff), local[i-1][j]+diff);
+                global[i][j] = Math.max(global[i-1][j], local[i][j]);
+            }
+        }
+        return global[len-1][2];
+    }
+
+    
     // Other solutions: 
     // https://leetcode.com/discuss/48151/my-c-solution-o-n-time-o-1-space-8ms
     // https://leetcode.com/discuss/18330/is-it-best-solution-with-o-n-o-1
