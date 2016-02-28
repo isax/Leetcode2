5fce7520bedd321321f798b7f274e438d955d8c2
isax <isax@users.noreply.github.com>
2016-02-28 10:18:08 -0800

    Update and rename SortList.java to 2222_SortList.java

--- 2222_SortList.java
+++ 2222_SortList.java
@@ -7,6 +7,41 @@
  * }
  */
 public class Solution {
+    // 2222
+     public ListNode sortList(ListNode head) {
+        if(head==null || head.next==null) return head;
+        
+        ListNode fast = head.next.next, slow = head; // otherwise stack overflow when [2,1]
+        while(fast!=null && fast.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        
+        ListNode right = slow.next;
+        slow.next = null;
+        return merge(sortList(head), sortList(right));
+    }
+    
+    private ListNode merge(ListNode left, ListNode right){
+        ListNode dummy = new ListNode(0);
+        ListNode head = dummy;
+        
+        while(left!=null && right!= null){
+            if(left.val <= right.val){
+                head.next = left;
+                left = left.next;
+            }else{
+                head.next = right;
+                right = right.next;
+            }
+            head = head.next;
+        }
+        head.next = left==null?right:left;
+        return dummy.next;
+    }
+    
+    
+    
     // https://leetcode.com/discuss/1709/have-pretty-mergesort-method-anyone-speed-reduce-memory-usage
     // http://blog.csdn.net/linhuanmars/article/details/21133949
     public ListNode sortList(ListNode head) {
