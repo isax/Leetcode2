b4c23027a47afaa800de35105b0d7e31ce029853
isax <isax@users.noreply.github.com>
2016-02-28 11:08:25 -0800

    Update and rename BestTimetoBuyandSellStockwithCooldown.java to 2222_BestTimetoBuyandSellStockwithCooldown.java

--- 2222_BestTimetoBuyandSellStockwithCooldown.java
+++ 2222_BestTimetoBuyandSellStockwithCooldown.java
@@ -6,6 +6,27 @@ public class Solution {
         int len = prices.length;
         if(len==0) return 0;
         
+        int [] s0 = new int[len];
+        int [] s1 = new int [len];
+        int [] s2 = new int [len];
+        
+        s1[0] = -prices[0];
+        s2[0] = 0;
+        
+        int max = 0;
+        for(int i = 1; i<len; i++){
+            s0[i] = Math.max(s2[i-1], s0[i-1]);
+            s1[i] = Math.max(s0[i-1] - prices[i], s1[i-1]);
+            s2[i] = s1[i-1] + prices[i];
+        }
+        return Math.max(s0[len-1], s2[len-1]);
+    }
+    
+    // https://leetcode.com/discuss/72030/share-my-dp-solution-by-state-machine-thinking
+    public int maxProfit(int[] prices) {
+        int len = prices.length;
+        if(len==0) return 0;
+        
         int[] s0 = new int[len];
         int[] s1 = new int[len];
         int[] s2 = new int[len];
