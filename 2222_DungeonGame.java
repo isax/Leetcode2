2ca4641cebeaef83356c5b47f8553219341c40fe
isax <isax@users.noreply.github.com>
2016-03-01 17:09:02 -0800

    Update and rename DungeonGame.java to 2222_DungeonGame.java

--- 2222_DungeonGame.java
+++ 2222_DungeonGame.java
@@ -1,4 +1,23 @@
 public class Solution {
+    
+    public int calculateMinimumHP(int[][] dungeon) {
+        int row = dungeon.length;
+        int col = dungeon[0].length;
+        int [][] dp = new int[row][col]; // life needed before entering i,j
+        
+        for(int i = row-1; i>=0; i--){
+            for(int j = col-1; j>=0; j--){
+                if(i==row-1 && j==col-1){
+                    dp[i][j] = Math.max(1, 1-dungeon[i][j]);
+                }else{
+                    dp[i][j] = Math.min((i+1<=row-1?dp[i+1][j]:Integer.MAX_VALUE), (j+1<=col-1?dp[i][j+1]:Integer.MAX_VALUE)) - dungeon[i][j];
+                    dp[i][j] = Math.max(1, dp[i][j]);
+                }
+            }
+        }
+        return dp[0][0];
+    }
+    
     // https://leetcode.com/discuss/47153/6-lines-16-ms-c-o-mn-time-o-n-space
     // https://leetcode.com/discuss/20721/my-ac-java-version-suggestions-are-welcome
     public int calculateMinimumHP(int[][] dungeon) {
