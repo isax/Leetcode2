240244ad8104ce07d783fcfc7226b186e049ab40
isax <isax@users.noreply.github.com>
2016-03-01 23:41:50 -0800

    Update and rename 2222_Subsets.java to 233_Subsets.java

--- 233_Subsets.java
+++ 233_Subsets.java
@@ -1,4 +1,24 @@
 public class Solution {
+    // 233
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> lists = new ArrayList<List<Integer>>();
+        Arrays.sort(nums);
+        helper(nums, 0, new ArrayList<Integer>(), lists);
+        return lists;
+    }
+    
+    private void helper(int [] nums, int i, List<Integer>list, List<List<Integer>> lists){
+        lists.add(new ArrayList<Integer>(list));
+        if(i==nums.length){
+            return;
+        }
+        for(int j = i; j<nums.length; j++){
+            list.add(nums[j]);
+            helper(nums, j+1, list, lists);
+            list.remove(list.size()-1);
+        }
+    }
+    
     // https://leetcode.com/discuss/54541/very-simple-and-fast-java-solution-with-explanation
     public List<List<Integer>> subsets(int[] nums) {
        Arrays.sort(nums);
