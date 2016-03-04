a8ecb4860538d41346688f7d7757356558de3478
isax <isax@users.noreply.github.com>
2016-03-03 19:58:28 -0800

    Update 2222_BinaryTreePaths.java

--- 2222_BinaryTreePaths.java
+++ 2222_BinaryTreePaths.java
@@ -8,6 +8,30 @@
  * }
  */
 public class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> list = new ArrayList<String>();
+        helper(root, new StringBuilder(), list);
+        return list;
+    }
+    
+    private void helper(TreeNode root, StringBuilder sb, List<String>list){
+        if(root==null) return;
+        
+        int oldLen = sb.length();
+        if(sb.length()!=0) {
+            sb.append("->");
+        }
+        sb.append(root.val);
+        
+        if(root.left==null && root.right==null){
+            list.add(sb.toString());
+            sb.setLength(oldLen);
+            return;
+        }
+        helper(root.left, sb, list);
+        helper(root.right, sb, list);
+        sb.setLength(oldLen);
+    }
     
     // DFS
     public List<String> binaryTreePaths(TreeNode root) {
