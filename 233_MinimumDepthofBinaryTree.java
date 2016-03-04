3009642bcee0d3e4c68a9a20b697649f56baa408
isax <isax@users.noreply.github.com>
2016-03-03 19:18:14 -0800

    Update and rename 2222_MinimumDepthofBinaryTree.java to 233_MinimumDepthofBinaryTree.java

--- 233_MinimumDepthofBinaryTree.java
+++ 233_MinimumDepthofBinaryTree.java
@@ -8,6 +8,38 @@
  * }
  */
 public class Solution {
+    // 233
+    public int minDepth(TreeNode root) {
+        if(root==null) return 0;
+        if(root.left==null && root.right==null) return 1;
+        
+        int minDepth = Integer.MAX_VALUE;
+        if(root.left!=null) minDepth = Math.min(minDepth, minDepth(root.left));
+        if(root.right!=null) minDepth = Math.min(minDepth, minDepth(root.right));
+        return minDepth + 1;
+    }
+    
+    public int minDepthBFS(TreeNode root) {
+        if(root==null) return 0;
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.add(root);
+        
+        int level = 1;
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i = 0; i<size; i++){
+                TreeNode node = q.poll();
+                if(node.left==null && node.right==null){
+                    return level;
+                }
+                if(node.left!=null) q.offer(node.left);
+                if(node.right!=null) q.offer(node.right);
+            }
+            level++;
+        }
+        return -1;
+    }
+    
     public int minDepthDFS(TreeNode root) {
         if(root==null) return 0;
         
