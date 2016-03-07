4bcffaf0a7eb6fbfdd3c542c1934fc20a102a411
isax <isax@users.noreply.github.com>
2016-03-06 16:17:33 -0800

    Update and rename OddEvenLinkedList.java to 2_OddEvenLinkedList.java

--- 2_OddEvenLinkedList.java
+++ 2_OddEvenLinkedList.java
@@ -7,6 +7,30 @@
  * }
  */
 public class Solution {
+    public ListNode oddEvenListMine(ListNode head) {
+        if(head==null || head.next==null) return head;
+        ListNode even = new ListNode(0);
+        ListNode evenHead = even;
+        ListNode odd = new ListNode(0);
+        ListNode oddHead = odd;
+        ListNode node = head;
+        
+        while(head!=null){
+            even.next = head;
+            even = even.next;
+            head = head.next;
+            
+            if(head!=null){ // there is odd
+                odd.next = head;
+                odd = odd.next;
+                head = head.next;
+            }
+        }
+        even.next = oddHead.next;
+        odd.next = null;
+        return evenHead.next;
+    }
+    
     public ListNode oddEvenList(ListNode head) {
         ListNode evenDummy = new ListNode(0);
         ListNode oddDummy = new ListNode(0);
