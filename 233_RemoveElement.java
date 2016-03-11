45533a9a8a5589ab62d0c3bc2e0f865066510c4c
isax <isax@users.noreply.github.com>
2016-03-10 19:44:23 -0800

    Update and rename 2222_RemoveElement.java to 233_RemoveElement.java

--- 233_RemoveElement.java
+++ 233_RemoveElement.java
@@ -1,4 +1,23 @@
 public class Solution {
+     public int removeElement(int[] nums, int val) {
+        int i = 0;
+        for(int j = 0; j<nums.length; j++){
+            if(nums[j] != val){
+                if(i!=j) {
+                    swap(nums, i, j);
+                }
+                i++;
+            }
+        }
+        return i;
+    }
+    
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
     // http://blog.csdn.net/linhuanmars/article/details/19965351
     public int removeElement(int[] nums, int val) {
         if(nums==null)  
