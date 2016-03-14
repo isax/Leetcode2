6dc63d2c33b9d2f1ed2510c5f2b3ff96d8181858
isax <isax@users.noreply.github.com>
2016-03-13 18:21:00 -0700

    Create 2222_KthLargestElementinanArray.java

---
+++ 2222_KthLargestElementinanArray.java
@@ -0,0 +1,38 @@
+public class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        return helper(nums, 0, nums.length-1, k);
+    }
+    
+    private int helper(int[] nums, int left, int right, int k){
+        int pivot = nums[left];
+        
+        int l = left, r = right;
+        while(l<=r){
+            while(l<=r && nums[l] >= pivot){
+                l++;
+            }
+            while(l<=r && nums[r] <= pivot){
+                r--;
+            }
+            if(l<=r){
+                swap(nums, l++, r--);
+            }
+        }
+        swap(nums, left, r);
+        
+        if(l-left==k){
+            return pivot;
+        }else if (l-left < k){
+            return helper(nums, l, right, k-(l-left));
+        }else{
+            //return helper(nums, left, l-2, k);
+            return helper(nums, left, r-1, k);
+        }
+    }
+    
+    private void swap(int[]nums, int left, int right){
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
+}
