440a8695d03c1d8350a07c75c2ba1e4610159377
isax <isax@users.noreply.github.com>
2016-03-13 23:10:44 -0700

    Update and rename 2222_ExpressionAddOperators.java to 322_ExpressionAddOperators.java

--- 322_ExpressionAddOperators.java
+++ 322_ExpressionAddOperators.java
@@ -1,4 +1,44 @@
 public class Solution {
+    //313
+    private void helper(String num, int target, long res, long pre, int cur, StringBuilder sb, List<String> list){
+        if(cur==num.length()){
+            if(res==target){
+                list.add(sb.toString());
+            }
+            return;
+        }
+        
+        long nextNum = 0;
+        for(int i = cur; i<num.length(); i++){
+            if(num.charAt(cur)=='0' && i!=cur) break;///////////
+            
+            nextNum = nextNum*10 + (num.charAt(i)-'0');
+            int len = sb.length();
+            
+            if(cur==0){/////////////
+                sb.append(nextNum);
+                helper(num, target, res+nextNum, nextNum, i+1, sb, list);
+                sb.setLength(len);
+                continue;
+            }
+            
+            //+
+            sb.append("+").append(nextNum);
+            helper(num, target, res+nextNum, nextNum, i+1, sb, list);
+            sb.setLength(len);
+            
+            //-
+            sb.append("-").append(nextNum);
+            helper(num, target, res-nextNum, -nextNum, i+1, sb, list);
+            sb.setLength(len);
+            
+            //*
+            sb.append("*").append(nextNum);
+            helper(num, target, res+pre*nextNum-pre, pre*nextNum, i+1, sb, list);
+            sb.setLength(len);
+        }
+    }
+    
     // https://github.com/isax/Leetcode2/blob/master/ExpressionAddOperators.java
     public List<String> addOperators(String num, int target) {
         List<String> list = new ArrayList<String>();
