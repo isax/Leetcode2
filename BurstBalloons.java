cea0105acad1dad3c6d458a3b4b6e4b5aafafba2
isax <isax@users.noreply.github.com>
2016-04-25 19:39:05 -0700

    Update BurstBalloons.java

--- BurstBalloons.java
+++ BurstBalloons.java
@@ -1,4 +1,35 @@
 public class Solution {
+    
+    public int maxCoins(int[] nums) {
+        int len = nums.length;
+        if(len==0) return 0;
+        int[] coins = new int[len + 2];
+        coins[0] = 1; coins[len + 1] = 1;
+        System.arraycopy(nums, 0, coins, 1, len);
+        
+        int[][] dp = new int[len+1][len+1];
+        
+        for(int l = 1; l<=len; l++){
+            for(int i = 1; i <= len - l + 1; i++){
+                int max = Integer.MIN_VALUE;
+                int j = i + l - 1;
+                for(int last = i; last <= j; last++){
+                    int reward = coins[last]*coins[i-1]*coins[i+l];
+                    if(last != i){
+                        reward += dp[i][last-1];
+                    }
+                    if(last != i + l -1){
+                        reward += dp[last+1][j];
+                    }
+                    max = Math.max(max, reward);
+                }
+                dp[i][j] = max;
+            }
+        }
+        return dp[1][len];
+    }
+    
+    
     // https://leetcode.com/discuss/72216/share-some-analysis-and-explanations
     public int maxCoins(int[] nums) {
         int len = nums.length;
